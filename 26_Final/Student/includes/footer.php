d08076efad6372b9d6dea078519d7c235b4cc117
chrisxjacobi <chrisxjacobi@gmail.com>
2017-10-19 12:49:02 -0500

    took info from index.php to include a header, footer, and nav php pages. used php include to reference from page to page. had issues with slugs/titles in arrays of pages, however, fixed with proper comma usage

---
+++ 26_Final/Student/includes/footer.php
@@ -0,0 +1,26 @@
+
+            <div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Phone</strong> 555.867.5309
+                </div>
+                <div class="column three">
+                    <strong>Location</strong> 123 Kapiolani Boulevard Honolulu, HI
+                </div>
+                <div class="column three last">
+                    <strong>Hours</strong>
+                    <em>Tuesday - Thursday</em><br> 1:00pm - 9:00pm<br><br>
+                    <em>Friday - Saturday</em><br> 4:00pm - 11:00pm<br><br>
+                    <em>Sunday - Monday</em><br> Closed
+                    <br><br>
+                </div>
+            </div>
+            <small>&copy;2014 Brad Hussey</small>
+        </div>
+    </div>
+    <div class="copyright-info">
+        <?php include('../assets/includes/copyright.php'); ?>
+    </div>
+</body>
+
+
+</html>
