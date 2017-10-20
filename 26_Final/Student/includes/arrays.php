91a96290b868dd5a11f388e7cbdc1e1226d8347e
chrisxjacobi <chrisxjacobi@gmail.com>
2017-10-20 13:09:35 -0500

    added dynamic menu items, with a tbc dish.php page. used a foreach loop to add items from our arrays

--- 26_Final/Student/includes/arrays.php
+++ 26_Final/Student/includes/arrays.php
@@ -47,4 +47,55 @@
     
     );
 
+
+    $menuItems = array(
+        "club-sandwich" => array(
+            title => "Club Sandwich ",
+            price => 11,
+            blurb => "Bacon ipsum dolor amet pig alcatra shank, sausage porchetta boudin tenderloin chicken. Frankfurter chicken salami, shoulder tri-tip biltong landjaeger jerky sausage bacon fatback rump pork chop. Corned beef prosciutto pork chop jowl ball tip ham sirloin pig tri-tip. Pork belly filet mignon short ribs corned beef, shank porchetta frankfurter boudin prosciutto andouille kevin meatball beef ribs jerky bacon.",
+            drink => "Club Soda"
+        ),
+        "dill-salmon" => array(
+            title => "Lemon &amp; Dill Salmon ",
+            price => 18,
+            blurb => "Bacon ipsum dolor amet pig alcatra shank, sausage porchetta boudin tenderloin chicken. Frankfurter chicken salami, shoulder tri-tip biltong landjaeger jerky sausage bacon fatback rump pork chop.",
+            drink => "Fancy Wine"
+        ),
+        "super-salad" => array(
+            title => "The Super Salad<sup>&reg;</sub> ",
+            price => 34,
+            blurb => "Pork belly filet mignon short ribs corned beef, shank porchetta frankfurter boudin prosciutto andouille kevin meatball beef ribs jerky bacon.",
+            drink => "Jug o' Water"
+        ),
+        "mexican-barbacoa" => array(
+            title => "Mexican Barbacoa ",
+            price => 23,
+            blurb => "Pork belly filet mignon short ribs corned beef, shank porchetta frankfurter boudin prosciutto andouille kevin meatball beef ribs jerky bacon.",
+            drink => "Beer with a lime"
+        ),
+        
+        
+    
+    
+    );
+
+
+    
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 26_Final/Student/team.php
+++ 26_Final/Student/team.php
@@ -9,9 +9,7 @@
     <p>We're small, but mighty. Franklin's Fine Dining has been a family-owned and run business since the dirty thirties, and we're proud of it! When you get these three together, you never know what can happen. But you can count on one thing: <strong>The best food you've ever had. Ever.</strong></p>
     <hr>
 
-    <?php 
-        foreach ($teamMembers as $member) {
-    ?>
+    <?php foreach ($teamMembers as $member) { ?>
 
         <div class="member">
             <img src="img/<?php echo $member[img] ?>.png" alt="<?php echo $member[name] ?>"
@@ -19,9 +17,7 @@
             <p><?php echo $member[bio] ?></p>
         </div>
 
-    <?php
-        }
-    ?>
+    <?php } ?>
 </div>
 <hr>
 
